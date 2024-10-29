ff7801bb9a30268f96add111678a8d523386af08
sandey2803 <sandeyoficialsnd@gmail.com>
2024-10-28 21:57:55 -0500

    Add tenant id

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
   features {}
   subscription_id = "a83d2d6f-296f-4780-b1d1-08684894c829"
+  tenant_id= "fc6213fd-6331-4522-b3ed-920648a31b26"
 }
 
 resource "azurerm_resource_group" "example" {
